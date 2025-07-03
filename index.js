d672b32f1fd5c7f03a35b529671e8df79b265ade
Sammaiah-Guguloth <gugulothsammaiah5@gmail.com>
2025-07-03 15:07:49 +0530

    corrected cors

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const allowedOrigins = [
   process.env.FRONTEND_BASE_URL,
   "http://localhost:5173", // Example for local testing
   "https://rebookhub-server.onrender.com",
-  "https://rebookhub1.netlify.app/about",
+  "https://rebookhub1.netlify.app",
   "https://686647556d4f6bb9af01ce2a--rebookhub1.netlify.app",
 ];
 
